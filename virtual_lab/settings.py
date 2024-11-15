d63c06e4b41c84168d16b2a74e8ee0210ed6b376
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-11-15 23:26:53 +0700

    .

--- virtual_lab/settings.py
+++ virtual_lab/settings.py
@@ -140,10 +140,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",  # Ganti dengan URL frontend Anda
-#     "https://virtual-lab-sorting-algorithm.vercel.app"
-# ]
+CORS_ALLOWED_ORIGINS = [
+    'https://frontend-indol-psi-15.vercel.app',
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
