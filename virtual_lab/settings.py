6f8e455b36337fb5923b6eb0a09a03575f3385e1
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-11-15 23:34:22 +0700

    .

--- virtual_lab/settings.py
+++ virtual_lab/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    'https://frontend-indol-psi-15.vercel.app',
+    'https://frontend-indol-psi-15.vercel.app/',
 ]
 
 REST_FRAMEWORK = {
