d6306a7284ca66203cad5ab58d5f6ab000b98941
steven-adrnn <131145320+steven-adrnn@users.noreply.github.com>
2024-11-15 23:54:19 +0700

    .

--- virtual_lab/settings.py
+++ virtual_lab/settings.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = 'virtual_lab.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASE_URL = os.getenv('DATABASE_URL', default='sqlite:///db.sqlite3')
+# DATABASE_URL = os.getenv('DATABASE_URL', default='sqlite:///db.sqlite3')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
